2070a407cdf22c581d4218ddbfe93411eeedf745
ksCaesar <x246libra@htomail.com>
2024-05-18 15:40:00 +0800

    refactor: UseEncodeJson

--- pkg/pubsub_handler.go
+++ pkg/pubsub_handler.go
@@ -9,11 +9,25 @@ import (
 func UseEncodeJson() art.Middleware {
 	return func(next art.HandleFunc) art.HandleFunc {
 		return func(message *art.Message, dependency any) error {
+			if message.Bytes != nil {
+				return nil
+			}
+
+			message.Mutex.Lock()
+
+			if message.Bytes != nil {
+				message.Mutex.Unlock()
+				return nil
+			}
+
 			bytes, err := json.Marshal(message.Body)
 			if err != nil {
+				message.Mutex.Unlock()
 				return err
 			}
+
 			message.Bytes = bytes
+			message.Mutex.Unlock()
 			return next(message, dependency)
 		}
 	}
